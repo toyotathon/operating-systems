c01a64101fa8bd5fd5a2beea7576013e39ca0e14
champagneabuelo <jev4zs@virginia.edu>
2017-04-12 23:43:13 -0400

    parsing values, tables, etc

--- project-3/vm.c
+++ project-3/vm.c
@@ -14,12 +14,22 @@ const int PM_SIZE = 128;
 /* masks to get lower 16 bits, page number, and offset */
 const uint16_t LOW_16_BITS = 0xFFFF;
 const uint16_t LOW_8_BITS = 0xFF;
-const uint16_t HIGH_8_BITS = 0xFF00;
+//const uint16_t HIGH_8_BITS = 0xFF00;
 
 int main(int argc, char *argv[]) {
 	bool checks;
 	FILE *addresses;
 	FILE *bs;
+	int framenum;
+
+	int physical_memory[PM_SIZE];
+	/*
+	virtual_memory initialization
+	[0] -> physical address
+	[1] -> age 
+	*/
+	int virtual_memory[VM_SIZE];
+	/* TODO implement TLB */
 
 	uint32_t address;
 	uint16_t lower;
@@ -48,17 +58,23 @@ int main(int argc, char *argv[]) {
 		exit(0);
 	}
 
+	/* initialize tables, etc */
+	int i;
+	for (i=0; i<PM_SIZE; i++) {
+		physical_memory[i] = i;
+	}
+	
+	for (i=0; i<VM_SIZE; i++) {
+		framenum = i*PAGE_SIZE;
+		virtual_memory[i] = framenum;
+	}
+
 	/* read in addresses, apply mask */
 	while ((read = getline(&value, &len, addresses)) != -1) {
 		address = strtol(value, &saveptr, 10);
-		lower = address && LOW_16_BITS;
-		pagenum =  lower && HIGH_8_BITS;
-		offset = lower && LOW_8_BITS;
-		printf("Address: %x\n", address);
-		printf("Lower bits: %x\n", lower);
-		printf("Page number: %x\n", pagenum);
-		printf("Offset: %x\n", offset);
-		printf("\n");
+		lower = address & LOW_16_BITS;
+		pagenum =  lower >> 8;
+				
 	}	
 	
 	fclose(addresses);
