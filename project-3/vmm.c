b0829f149ab0210e4e6ffd20ee2d6aae00c78df7
champagneabuelo <jev4zs@virginia.edu>
2017-04-15 19:31:51 -0400

    page table implemented, now on to tlb

--- project-3/vmm.c
+++ project-3/vmm.c
@@ -7,21 +7,75 @@
 /* header files for parsing, other things coming */
 #include "parsing.h"
 
-#define PAGE_TABLE 8
-#define PAGE_SIZE 16
+#define PAGE_TABLE 16
+#define FRAMES 8
+#define TLB_ENTRIES 4
 
-/* masks to get lower 16 bits, page number, and offset */
-const uint16_t LOW_16_BITS = 0xFFFF;
-const uint16_t LOW_8_BITS = 0xFF;
+/* masks to get page number and offset */
+const uint16_t PAGE_NUMBER_BITS = 0xF00;
+const unsigned COUNTER = 0x8000;
+//const uint16_t OFFSET_BITS = 0xFF;
+
+/* 
+2 structs to use:
+- TLB block
+	- page number
+	- frame number
+- Page block
+	- page 
+	- counter for LRU replacement
+	- bool to indicate that it is in memory
+*/
 
 typedef struct {
 	int page;
 	int frame;
-} block;
+} tlbBlock;
 
-block blockInit() {
-	block newBlock = {0,0};
-	return newBlock;
+tlbBlock newTLBBlock(int page, int frame) {
+	tlbBlock tlb;
+	tlb.page = page;
+	tlb.frame = frame;
+	return tlb;
+}
+
+typedef struct {
+	int page;
+	bool inmem;
+	unsigned counter;	
+} pageBlock;
+
+pageBlock pageBlockInit() {
+	pageBlock p;
+	p.page = 0;
+	p.inmem = false;
+	p.counter = 0;
+	return p;
+}
+
+pageBlock newPageBlock(int page) {
+	pageBlock p;
+	p.page = page;
+	p.inmem = true;
+	p.counter = 0;
+	return p;
+}
+
+/* data structures for vmm */
+int physicalMemory[FRAMES][256];
+bool freeFrames[FRAMES]; 
+pageBlock pageTable[PAGE_TABLE];
+tlbBlock tlb[TLB_ENTRIES]; // TODO implement TLB
+
+int currentFreeFrame() {
+	int i;
+	for (i=0; i<FRAMES; i+=1) {
+		if (freeFrames[i] == true) {
+			freeFrames[i] = false;
+			return i;
+		}
+	}
+	return -1;
 }
 
 int main(int argc, char *argv[]) {
@@ -29,9 +83,7 @@ int main(int argc, char *argv[]) {
 	FILE *addresses;
 	FILE *bs;
 	
-	/* data structures for vmm */
-	int physicalMemory[PAGE_TABLE][256];
-	block pageTable[PAGE_TABLE] = {blockInit()};
+	/* variables for pointing, iterating through data structure */
 
 	/* variables for data display */
 	int total = 0;
@@ -39,10 +91,9 @@ int main(int argc, char *argv[]) {
 	int hits = 0;
 
 	/* variables for parsing the address */
-	uint32_t address;
-	uint16_t lower;
-	uint8_t pagenum;
-	uint8_t offset;
+	int address;	
+	int pagenum;
+	//int offset;
 
 	/* variables for iterating through txt file */
 	char *value = NULL;
@@ -65,16 +116,93 @@ int main(int argc, char *argv[]) {
 	} else {
 		exit(0);
 	}
+	
+	int i;
+	/* initializing page table contents */
+	for (i=0; i<PAGE_TABLE ;i+=1) {
+		pageTable[i] = pageBlockInit();
+	}
+
+	/* initializing free frame list */
+	for (i=0; i<FRAMES; i+=1) {
+		freeFrames[i] = true;
+	}
 
 	/* read in addresses, apply mask */
 	while ((read = getline(&value, &len, addresses)) != -1) {
+		total += 1;
 		address = strtol(value, &saveptr, 10);
-		lower = address & LOW_16_BITS;
-		pagenum =  lower >> 8;
-		offset = lower & LOW_8_BITS;
+		pagenum = (address & PAGE_NUMBER_BITS) >> 8;
+		
+		printf("Address: %d\n", address);
+		printf("Page number: %d\n", pagenum);
+		
+		// page is NOT in memory
+		if (!pageTable[pagenum].inmem) {
+			fault += 1;
+			// TODO error message here
+			printf("Virtual address %d contained in page %d causes a page fault.\n",address,pagenum);
+			// check free frame list
+			int free = currentFreeFrame();
+			if (free == -1) { 
+				// do something to free up physical mem
+				int framereplace;
+				unsigned min;
+				min = pageTable[0].counter;
+				int replace = 0;
+				for (i=1; i<PAGE_TABLE; i+=1) {
+					if (pageTable[i].counter < min){
+						min = pageTable[i].counter;
+						replace = i;
+						printf("getting reached");
+					} 
+				}
+				printf("Page %d is being replaced.\n", replace);
+				pageTable[replace].inmem = false;
+				framereplace = pageTable[replace].page;
 				
-	}	
+				pageTable[pagenum].page = framereplace;
+				pageTable[pagenum].inmem = true;
+				pageTable[pagenum].counter |= COUNTER;	
+
+				int j;
+				int data;
+				for (j=0; j< 265; j+=1) {
+					fseek(bs, pagenum*256+j*4, SEEK_SET);
+					fread(&data, sizeof(data), 1, bs);
+					physicalMemory[framereplace][j] = data;
+				}
+			} 
+			else {
+				// set the page value in page table
+				pageTable[pagenum].page = free;
+				pageTable[pagenum].inmem = true;
+				pageTable[pagenum].counter |= COUNTER;
+
+				// set physical memory
+				int j;
+				int data;
+				for (j=0; j< 265; j+=1) {
+					fseek(bs, pagenum*256+j*4, SEEK_SET);
+					fread(&data, sizeof(data), 1, bs);
+					physicalMemory[free][j] = data;
+				}
+			}	
+		} 
 	
+		// page is in memory
+		else {
+			hits += 1;
+			pageTable[pagenum].counter |= COUNTER;
+			// TODO message here 	
+		}
+		// shift bits to move clock
+		for (i=0; i<PAGE_TABLE; i+=1) {
+			pageTable[i].counter >>= 1;	
+		}
+		//for (i=0; i<PAGE_TABLE; i+=1) {
+		//	printf("Page %d count: %d\n", i, pageTable[i].counter);
+		//}
+	}	
 	fclose(addresses);
 }
-
